8e475564ce02e1ded24567f7523afc6739e84ae2
Marco Kuidja <kuidjamarco@gmail.com>
2024-01-23 11:10:02 +0100

    feat(shared): #2 provided user entity data type

--- libs/data-types/src/lib/data-types/user.d.ts
+++ libs/data-types/src/lib/data-types/user.d.ts
@@ -1,11 +1,17 @@
-import { SchemaRequestBody } from '../api.helper';
+import { SchemaRequestBody, SchemaResponseBody } from '../api.helper';
 import { operations } from '../docs';
 
 export type SignUpPayload = SchemaRequestBody<
   operations,
   'UsersController_createUser'
 >;
+
 export type SignInPayload = SchemaRequestBody<
   operations,
   'UsersController_validateUser'
+>;
+
+export type UserEntity = SchemaResponseBody<
+  operations,
+  'UsersController_getOne'
 >;
