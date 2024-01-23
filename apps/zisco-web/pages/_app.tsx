13a0adf147feb3fc0abdb41bcd3e3a071d450132
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-01-23 15:25:00 +0100

    feat(app): #4 configured and tested language swapper

--- apps/zisco-web/pages/_app.tsx
+++ apps/zisco-web/pages/_app.tsx
@@ -1,12 +1,52 @@
 import { ThemeProvider } from '@emotion/react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { IntlProvider } from 'react-intl';
 import '../public/styles/global.scss';
 import '../public/styles/notifGlobalStyles.css';
 import '../public/styles/phoneNumberStyles.css';
 import '../public/styles/reset.css';
 import '../public/styles/root.scss';
+import LanguageContextProvider, {
+  usePreferredLanguage,
+} from '../utils/contexts/language/LanguageContextProvider';
 import { theme } from '../utils/theme';
+
+import { ReactNode } from 'react';
+import enMessages from '../utils/languages/en-us';
+import esMessages from '../utils/languages/es';
+import frMessages from '../utils/languages/fr';
+import ruMessages from '../utils/languages/ru';
+
+export function TempApp({ children }: { children: ReactNode }) {
+  const preferredLanguage = usePreferredLanguage();
+  const activeMessages = getActiveLanguage();
+
+  function getActiveLanguage() {
+    switch (preferredLanguage) {
+      case 'en':
+        return enMessages;
+      case 'fr':
+        return frMessages;
+      case 'es':
+        return esMessages;
+      case 'ru':
+        return ruMessages;
+      default:
+        return enMessages;
+    }
+  }
+
+  return (
+    <IntlProvider
+      messages={activeMessages}
+      locale={preferredLanguage}
+      defaultLocale="en"
+    >
+      {children}
+    </IntlProvider>
+  );
+}
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -15,9 +55,13 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>ZiscoERP - Powerful HR, Accounting, CRM System</title>
       </Head>
       <main className="app">
-        <ThemeProvider theme={theme}>
-          <Component {...pageProps} />
-        </ThemeProvider>
+        <LanguageContextProvider>
+          <TempApp>
+            <ThemeProvider theme={theme}>
+              <Component {...pageProps} />
+            </ThemeProvider>
+          </TempApp>
+        </LanguageContextProvider>
       </main>
     </>
   );
